efaa8e51697be03c1323b3bd7a58f7fd9a571288
BALIST0N <BALIST0N@users.noreply.github.com>
2020-02-28 06:34:57 +0100

    add icon and remove manual globals

--- EFT_Launcher_12/Source/Globals.cs
+++ EFT_Launcher_12/Source/Globals.cs
@@ -8,21 +8,19 @@ namespace EFT_Launcher_12
 		public static string gameFolder;
 		public static string serverFolder;
 		public static string profilesFolder;
-		public static string version;
 		public static bool launchServer;
 		public static bool useServerPath;
 		public static ClientConfig clientConfig;
 		
 		static Globals()
 		{
-			//serverFolder = Environment.CurrentDirectory;
-			//profilesFolder = Path.Combine(Environment.CurrentDirectory, "user");
+			serverFolder = Environment.CurrentDirectory;
+			profilesFolder = Path.Combine(Environment.CurrentDirectory, "user");
 
-			serverFolder = "Y:/tarkov/emutarkov git/EmuTarkov-Server";
-			profilesFolder = "Y:/tarkov/emutarkov git/EmuTarkov-Server/user";
+			//serverFolder = "Y:/tarkov/emutarkov git/EmuTarkov-Server";
+			//profilesFolder = "Y:/tarkov/emutarkov git/EmuTarkov-Server/user";
 
 			gameFolder = Properties.Settings.Default.gameFolder;
-			version = "EmuTarkov Launcher 12";
 			launchServer = true;
 			clientConfig = null;
 		}
--- EFT_Launcher_12/Views/MainWindow.Designer.cs
+++ EFT_Launcher_12/Views/MainWindow.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.label1 = new System.Windows.Forms.Label();
             this.startButton = new System.Windows.Forms.Button();
             this.profilesListBox = new System.Windows.Forms.ComboBox();
@@ -156,10 +157,12 @@
             this.Controls.Add(this.profilesListBox);
             this.Controls.Add(this.startButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(600, 400);
             this.MinimumSize = new System.Drawing.Size(600, 160);
             this.Name = "MainWindow";
+            this.Text = "Emutarkov Launcher (Alternative Version)";
             this.ResumeLayout(false);
             this.PerformLayout();
 
